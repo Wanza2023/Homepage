f6c4f5f958b69645e76c4e62cf591c9ba8673525
yejinkang <yejin331@daum.net>
2023-09-22 16:18:22 +0900

    진짜진짜최종

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,88 @@
-import React from "react";
-import ProImg from "../assets/images/project.png";
+import { useState } from "react";
 import Foot from "./Foot";
+import "../styles/Project.css";
+
+const Data = [
+    {
+        month: 9,
+        content: [
+            '팀 결성',
+            '홈페이지 제작',
+        ],
+    },
+    {
+        month: 10,
+        content: [
+            '블로그 개발 예정'
+        ],
+    },
+    {
+        month: 11,
+        content: [
+            
+        ],
+    },
+    {
+        month: 12,
+        content: [
+            
+        ],
+    },
+    {
+        month: 1,
+        content: [
+            
+        ],
+    },
+    {
+        month: 2,
+        content: [
+            
+        ],
+    },
+];
 
 function Project(){
-    return(
+    const [selectedMonth, setSelectedMonth] = useState(9);
+    
+    const handleMonthChange = (month) => {
+        setSelectedMonth(month);
+    };
+
+    return (
         <div style={{paddingTop: "4rem"}}>
-            <img style={{marginBottom: "0rem", padding: "0rem"}} 
-            alt="" src={ProImg} height="400px" width="750px"/>
-            <Foot/>
+            <div className="projectContainer">
+                <h2>프로젝트</h2>
+                <div className="monthSelector">
+                    <ul>
+                    {Data.map((item) => (
+                        <li
+                        key={item.month}
+                        onClick={() => handleMonthChange(item.month)}
+                        className={selectedMonth === item.month ? 'active' : ''}
+                        >
+                        {item.month}
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+                <div className={`projectContent ${selectedMonth === 9 || selectedMonth === 10 ? 'horizontal' : ''}`}>
+                    <h3>{selectedMonth}월</h3>
+                    <ul>
+                    {Data.find((item) => item.month === selectedMonth)?.content.map((event, index) => (
+                        <li key={index}>
+                        {event}
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <Foot/>
+
+            </div>
         </div>
-    );  
+    );
 }
 
 export default Project;
